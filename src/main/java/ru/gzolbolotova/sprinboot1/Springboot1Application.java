f9907eea961ecf3d1fae6307bc74836e6c5015b9
gzolbolotova <67222280+gzolbolotova@users.noreply.github.com>
2023-08-27 19:00:04 +0300

    hi

--- src/main/java/ru/gzolbolotova/sprinboot1/Springboot1Application.java
+++ src/main/java/ru/gzolbolotova/sprinboot1/Springboot1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sprinboot1Application {
+public class Springboot1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Sprinboot1Application.class, args);
+		SpringApplication.run(Springboot1Application.class, args);
 	}
 
 }
